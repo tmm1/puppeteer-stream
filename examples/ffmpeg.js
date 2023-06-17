ca57a1c485a5147d392a066fa410fb355fc2b8a8
Aman Karmani <aman@tmm1.net>
2023-06-16 17:57:23 -0700

    better fullscreen

--- examples/ffmpeg.js
+++ examples/ffmpeg.js
@@ -21,18 +21,41 @@ async function test() {
 			"--hide-crash-restore-bubble",
 			"--user-data-dir=./chromedata",
 		],
-		ignoreDefaultArgs: ["--enable-automation"],
+		ignoreDefaultArgs: ["--enable-automation", "--disable-component-update"],
 	});
 
 	const page = await browser.newPage();
 	//await page.goto("https://www.nbc.com/live?brand=nbc-news&callsign=nbcnews");
 	await page.goto("https://www.nbc.com/live?brand=cnbc&callsign=cnbc");
 	await page.waitForSelector("video");
+	await page.waitForFunction(() => {
+		let video = document.querySelector("video");
+		return video.readyState === 4;
+	});
 	await page.evaluate(() => {
 		let video = document.querySelector("video");
-		video.requestFullscreen();
+		//video.requestFullscreen();
+		//document.querySelector("button[data-name=fullscreen]").click();
+		video.style.position = "fixed";
+		video.style.top = "0";
+		video.style.left = "0";
+		video.style.width = "100%";
+		video.style.height = "100%";
+		video.style.zIndex = "999000";
+		video.style.background = "black";
 		video.play();
 	});
+
+	const session = await page.target().createCDPSession();
+	const { windowId } = await session.send("Browser.getWindowForTarget");
+	await session.send("Browser.setWindowBounds", {
+		windowId,
+		bounds: {
+			height: viewport.height + 77,
+			width: viewport.width,
+		},
+	});
+
 	const stream = await getStream(page, {
 		audio: true,
 		video: true,
@@ -56,12 +79,12 @@ async function test() {
 
 	stream.pipe(ffmpeg.stdin);
 
-	setTimeout(async () => {
+	process.on("SIGINT", async () => {
 		await stream.destroy();
 		ffmpeg.kill("SIGINT");
 
 		console.log("finished");
-	}, 1000 * 120);
+	});
 }
 
 test();
